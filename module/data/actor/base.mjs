d686ac1e0cdc1ce88a6952c3d9641bea12c3d951
WBHarry <89362246+WBHarry@users.noreply.github.com>
2025-08-24 18:40:06 +0200

    . (#1056)

--- module/data/actor/base.mjs
+++ module/data/actor/base.mjs
@@ -115,11 +115,16 @@ export default class BaseDataActor extends foundry.abstract.TypeDataModel {
             const typeForDefeated = ['character', 'adversary', 'companion'].find(x => x === this.parent.type);
             if (defeatedSettings.enabled && typeForDefeated) {
                 const resource = typeForDefeated === 'companion' ? 'stress' : 'hitPoints';
-                if (changes.system.resources[resource]) {
-                    const becameMax = changes.system.resources[resource].value === this.resources[resource].max;
+                const resourceValue = changes.system.resources[resource];
+                if (
+                    resourceValue &&
+                    this.resources[resource].max &&
+                    resourceValue.value !== this.resources[resource].value
+                ) {
+                    const becameMax = resourceValue.value === this.resources[resource].max;
                     const wasMax =
                         this.resources[resource].value === this.resources[resource].max &&
-                        this.resources[resource].value !== changes.system.resources[resource].value;
+                        this.resources[resource].value !== resourceValue.value;
                     if (becameMax) {
                         this.parent.toggleDefeated(true);
                     } else if (wasMax) {
