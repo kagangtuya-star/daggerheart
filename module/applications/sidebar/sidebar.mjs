55586c93c837662124ebc95b8d4193215a9c0b1d
Chris Ryan <73275196+chrisryan10@users.noreply.github.com>
2025-09-19 06:27:04 +1000

    Move Foundryborne menu location; fix localisation; fix deprecation (#1169)
    Co-authored-by: Chris Ryan <chrisr@blackhole>

--- module/applications/sidebar/sidebar.mjs
+++ module/applications/sidebar/sidebar.mjs
@@ -1,10 +1,49 @@
-export default class DhSidebar extends Sidebar {
+export default class DhSidebar extends foundry.applications.sidebar.Sidebar {
     /** @override */
     static TABS = {
-        ...super.TABS,
+        chat: {
+            documentName: 'ChatMessage'
+        },
+        combat: {
+            documentName: 'Combat'
+        },
+        scenes: {
+            documentName: 'Scene',
+            gmOnly: true
+        },
+        actors: {
+            documentName: 'Actor'
+        },
+        items: {
+            documentName: 'Item'
+        },
+        journal: {
+            documentName: 'JournalEntry',
+            tooltip: 'SIDEBAR.TabJournal'
+        },
+        tables: {
+            documentName: 'RollTable'
+        },
+        cards: {
+            documentName: 'Cards'
+        },
+        macros: {
+            documentName: 'Macro'
+        },
+        playlists: {
+            documentName: 'Playlist'
+        },
+        compendium: {
+            tooltip: 'SIDEBAR.TabCompendium',
+            icon: 'fa-solid fa-book-atlas'
+        },
         daggerheartMenu: {
             tooltip: 'DAGGERHEART.UI.Sidebar.daggerheartMenu.title',
             img: 'systems/daggerheart/assets/logos/FoundryBorneLogoWhite.svg'
+        },
+        settings: {
+            tooltip: 'SIDEBAR.TabSettings',
+            icon: 'fa-solid fa-gears'
         }
     };
 
