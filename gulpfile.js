6a2d09caac5c076b1ea76ecf7352f722160c82d8
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2026-05-19 17:18:10 -0400

    Create sourcemaps for css files (#1904)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 // Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('less', function (cb) {
-    gulp.src('styles/daggerheart.less').pipe(less()).on('error', console.error.bind(console)).pipe(gulp.dest('styles'));
+    gulp.src('styles/daggerheart.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .on('error', console.error.bind(console))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('styles'));
     cb();
 });
 
