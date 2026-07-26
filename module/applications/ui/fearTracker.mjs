acb6d5052f7c08d2d7a52548bea90e8fd801da12
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2026-07-26 06:28:30 -0400

    Reduce top center fear tracker shifting on lower resolutions (#2115)

--- module/applications/ui/fearTracker.mjs
+++ module/applications/ui/fearTracker.mjs
@@ -149,11 +149,12 @@ export default class FearTracker extends HandlebarsApplicationMixin(ApplicationV
             const top = document.getElementById('ui-top');
 
             // Logic derived from the internal code's ChatLog#offsetHotbar(). Since the sidebar might be mid animation, we can't really check directly.
+            // Treat effects as always open to minimize UI shifting
             const { uiScale } = game.settings.get('core', 'uiConfig');
             const sidebarWidth = (348 * ui.sidebar.expanded) / uiScale;
             const countdownsWidth = countdowns.getBoundingClientRect().width;
-            const effectsWidth = effectsDisplay.getBoundingClientRect().width;
-            const rightWidth = sidebarWidth + countdownsWidth + 16 + (effectsWidth ? 62 : 0);
+            const effectsWidth = Math.max(46, effectsDisplay.getBoundingClientRect().width);
+            const rightWidth = sidebarWidth + countdownsWidth + 16 + effectsWidth + 16;
             const countdownLeftEdge = window.innerWidth - rightWidth - 16; // w/ extra padding
 
             const topBounds = top.getBoundingClientRect();
