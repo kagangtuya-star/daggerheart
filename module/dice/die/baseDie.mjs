5b3c7e32d6bd7dcc1c6a6941aa637701a5f52f69
WBHarry <89362246+WBHarry@users.noreply.github.com>
2026-07-30 22:08:00 +0200

    [Housekeeping] Remove Compound ComboDice (#2131)
    * Removed compound combo dice since the mechanic was scrapped
    
    * .

--- module/dice/die/baseDie.mjs
+++ module/dice/die/baseDie.mjs
@@ -1,9 +1,8 @@
-import { adjustDice, triggerChatRollFx } from '../../helpers/utils.mjs';
+import { triggerChatRollFx } from '../../helpers/utils.mjs';
 
 export default class BaseDie extends foundry.dice.terms.Die {
     static MODIFIERS = {
         ...foundry.dice.terms.Die.MODIFIERS,
-        cc: 'compoundComboDice',
         c: 'comboDice'
     };
 
@@ -39,28 +38,20 @@ export default class BaseDie extends foundry.dice.terms.Die {
     /*  Modifier Logic                              */
     /* -------------------------------------------- */
 
-    async compoundComboDice() {
-        return this.handleComboDice({ maxIncreasesDiceSize: true});
-    }
-
     async comboDice() {
-        return this.handleComboDice({ maxIncreasesDiceSize: false });
-    }
-
-    async handleComboDice(maxIncreasesDiceSize) {
         /* ComboDice only works with exactly two dice and both have to be the same denomination */
         if (this.number !== 2) {
             ui.notifications.warn(game.i18n.localize('DAGGERHEART.UI.Notifications.comboDiceOnlyTwoDiceError'));
             return false;
         }
 
-        return this.rollComboDice(maxIncreasesDiceSize);
+        return this.rollComboDice();
     }
 
-    async rollComboDice(options) {
-        const { maxIncreasesDiceSize, rerollStartIndex } = options;
+    async rollComboDice(options = {}) {
+        const { rerollStartIndex } = options;
         const initialResultsLength = this.results.filter(x => x.active).length;
-        const result = await this.continueCombo(maxIncreasesDiceSize);
+        const result = await this.continueCombo();
 
         /* The flow of DiceSoNice has no way of knowing that some of the results of a Die should be a different denomination 
            We solve this by marking the results as hidden so they're not picked up by the auto roll of DiceSoNice.
@@ -87,14 +78,13 @@ export default class BaseDie extends foundry.dice.terms.Die {
         for (const result of this.results)
             result.hidden = true;
 
-        const modifier = maxIncreasesDiceSize ? 'cc' : 'c';
-        if (!this.modifiers.includes(modifier))
-            this.modifiers.push(modifier);
+        if (!this.modifiers.includes('c'))
+            this.modifiers.push('c');
 
         return result;
     }
 
-    async continueCombo(maxIncreasesDiceSize) {
+    async continueCombo() {
         const activeResults = this.results.filter(x => x.active);
         const lastIndex = activeResults.length - 1;
         const lastResult = activeResults[lastIndex];
@@ -105,27 +95,20 @@ export default class BaseDie extends foundry.dice.terms.Die {
         const lastFaces = lastResult.denomination ? 
             Number(lastResult.denomination.slice(1)) : this.faces;
         const currentDenomination = `d${lastFaces}`
-        const increaseDiceSize = 
-            maxIncreasesDiceSize && lastFaces < 12 && 
-            lastResult.result === lastFaces;
 
-        const denomination = increaseDiceSize ? adjustDice(currentDenomination, false) : currentDenomination;
-
-        const newRoll = await (new Roll(`1${denomination}`)).evaluate();
-        this.results.push({ result: newRoll.total, denomination: denomination, active: true });
+        const newRoll = await (new Roll(`1${currentDenomination}`)).evaluate();
+        this.results.push({ result: newRoll.total, denomination: currentDenomination, active: true });
         this.number += 1;
 
-        return this.continueCombo(maxIncreasesDiceSize);
+        return this.continueCombo();
     }
 
-    async handleComboDiceReroll(rerolledIndex, originalResult) {
-        /* Potentially sliced when correcting compound dice at (1) */
-        let resultGroupingIndexes = this.results.map((x, index) => ({ index, active: x.active }))
+    async handleComboDiceReroll(rerolledIndex) {
+        const resultGroupingIndexes = this.results.map((x, index) => ({ index, active: x.active }))
             .filter(x => x.active).map(x => x.index);
         if (resultGroupingIndexes.length <= 1) return;
 
         const rerolledResult = this.results[rerolledIndex];
-        const compoundCombo = this.modifiers.includes('cc');
         const rerollGroupingIndex = resultGroupingIndexes.indexOf(rerolledIndex);
 
         const previousIndex = resultGroupingIndexes[rerollGroupingIndex - 1];
@@ -139,38 +122,21 @@ export default class BaseDie extends foundry.dice.terms.Die {
             this.number = this.results.filter(x => x.active).length;
         };
 
-        /* (1) If subsequent size increases from the compound effect are now incorrect, drop subsequent dice */
-        const lastFaces = Number((originalResult.denomination ?? this.denomination).slice(1));
-        if (
-            compoundCombo && 
-            (
-                (originalResult.result === lastFaces && 
-                rerolledResult.result !== lastFaces)
-                ||
-                (originalResult.result !== lastFaces &&
-                rerolledResult.result === lastFaces)
-            )
-        ) {
-            dropDice(false);
-            resultGroupingIndexes = resultGroupingIndexes.slice(0, this.number);
-        }
-
-        /* (2) Rerolling any of the last two dice might introduce new results */
+        /* (1) Rerolling any of the last two dice might introduce new results */
         const isFinalHigher = 
             rerollGroupingIndex === resultGroupingIndexes.length - 1 && rerolledResult.result >= previousResult?.result;
         const isSemifinalLower = 
             rerollGroupingIndex === resultGroupingIndexes.length - 2 && rerolledResult.result < nextResult?.result;
         if (isFinalHigher || isSemifinalLower) {
             return await this.rollComboDice({
-                maxIncreasesDiceSize: compoundCombo,
                 rerollStartIndex: rerollGroupingIndex
             });
         }
-        /* (3) Rerolling a subsequent dice might invalidate later dice which should then be dropped */
+        /* (2) Rerolling a subsequent dice might invalidate later dice which should then be dropped */
         else if (rerolledResult.result < previousResult?.result){
             dropDice(false);
         }
-        /* (4) Rerolling a preceeding dice might invalidate later dice which should then be dropped */
+        /* (3) Rerolling a preceeding dice might invalidate later dice which should then be dropped */
         else if (rerolledResult.result >= nextResult?.result) {
             dropDice(true);
         }
