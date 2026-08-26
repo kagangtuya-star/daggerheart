fd5944a7c1c4dcbaae9a4b7d28ed442e64380218
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2026-08-26 03:03:12 -0400

    Remove rsquo and friends from more properties (#2250)

--- tools/pushLDBtoYML.mjs
+++ tools/pushLDBtoYML.mjs
@@ -51,13 +51,23 @@ function transformDocument(entry) {
     const stats = entry._stats;
     entry._stats = stats ? { compendiumSource: stats.compendiumSource } : stats;
     delete entry.ownership;
+    entry.name = removeSpecialCharacters(entry.name);
     entry.description = removeSpecialCharacters(entry.description);
-    if (entry?.system) {
+    if (entry.system) {
+        entry.system.motivesAndTactics = removeSpecialCharacters(entry.system.motivesAndTactics);
         entry.system.description = removeSpecialCharacters(entry.system.description);
+        entry.system.backgroundQuestions = entry.system.backgroundQuestions?.map(removeSpecialCharacters);
+        entry.system.connections = entry.system.connections?.map(removeSpecialCharacters);
+        if (entry.system.duration) {
+            entry.system.duration.description = removeSpecialCharacters(entry.system.duration.description);
+        }
         for (const action of Object.values(entry.system.actions ?? {})) {
             action.description = removeSpecialCharacters(action.description);
             if (action.description && action.description === entry.system.description) {
                 action.description = '';
+            }
+            for (const area of action.areas ?? []) {
+                area.name = removeSpecialCharacters(area.name)
             }
         }
 
@@ -65,6 +75,9 @@ function transformDocument(entry) {
         if ('changes' in entry.system && 'origin' in entry) {
             delete entry.origin;
         }
+    }
+    if (entry.prototypeToken) {
+        entry.prototypeToken.name = removeSpecialCharacters(entry.prototypeToken.name);
     }
     
     for (const effect of entry.effects ?? []) {
