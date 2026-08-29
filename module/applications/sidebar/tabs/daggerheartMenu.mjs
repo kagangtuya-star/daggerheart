96f556739574b25c79af6e4174af821cd3fafdf2
WBHarry <89362246+WBHarry@users.noreply.github.com>
2026-08-29 21:51:54 +0200

    Added buttosn for GM Downtime Consequences in the Daggerheart sidebar menu (#2288)

--- module/applications/sidebar/tabs/daggerheartMenu.mjs
+++ module/applications/sidebar/tabs/daggerheartMenu.mjs
@@ -30,9 +30,10 @@ export default class DaggerheartMenu extends HandlebarsApplicationMixin(Abstract
             title: 'SIDEBAR.TabSettings'
         },
         actions: {
-            selectRefreshable: DaggerheartMenu.#selectRefreshable,
-            refreshActors: DaggerheartMenu.#refreshActors,
-            createFallCollisionDamage: DaggerheartMenu.#createFallCollisionDamage
+            selectRefreshable: DaggerheartMenu.#onSelectRefreshable,
+            refreshActors: DaggerheartMenu.#onRefreshActors,
+            createFallCollisionDamage: DaggerheartMenu.#onCreateFallCollisionDamage,
+            rollDowntimeFear: DaggerheartMenu.#onRollDowntimeFear
         }
     };
 
@@ -60,13 +61,13 @@ export default class DaggerheartMenu extends HandlebarsApplicationMixin(Abstract
     /*  Application Clicks Actions                  */
     /* -------------------------------------------- */
 
-    static async #selectRefreshable(_event, button) {
+    static async #onSelectRefreshable(_event, button) {
         const { type } = button.dataset;
         this.refreshSelections[type].selected = !this.refreshSelections[type].selected;
         this.render();
     }
 
-    static async #refreshActors() {
+    static async #onRefreshActors() {
         const refreshKeys = Object.keys(this.refreshSelections).filter(key => this.refreshSelections[key].selected);
         await RefreshFeatures(refreshKeys);
 
@@ -74,7 +75,7 @@ export default class DaggerheartMenu extends HandlebarsApplicationMixin(Abstract
         this.render();
     }
 
-    static async #createFallCollisionDamage(_event, button) {
+    static async #onCreateFallCollisionDamage(_event, button) {
         const data = CONFIG.DH.GENERAL.fallAndCollisionDamage[button.dataset.key];
         const roll = new Roll(data.damageFormula);
         await roll.evaluate();
@@ -90,5 +91,65 @@ export default class DaggerheartMenu extends HandlebarsApplicationMixin(Abstract
             rolls: [rollJSON],
             sound: CONFIG.sounds.dice
         });
+    }
+
+    static async #onRollDowntimeFear(_event, button) {
+        let formula;
+        if (button.dataset.type === 'short') {
+            formula = '1d4';
+        } else {
+            const activeParty = game.actors.find(x => x.type === 'party' && x.system.active);
+            const partySize = activeParty?.system.partyMembers.length ?? await this.getFallbackPartySize();
+            if (!partySize) return;
+            
+            if (button.dataset.type === 'long') {
+                formula = `1d4 + ${partySize}`;
+            } else {
+                formula = `${partySize}d6`;
+            }
+        }
+
+        if (!formula) return;
+
+        const fearRoll = await (new Roll(formula)).evaluate();
+        const message = await getDocumentClass('ChatMessage').create({
+            user: game.user.id,
+            title: _loc('DAGGERHEART.APPLICATIONS.DaggerheartMenu.chatMessageTitle'),
+            flavor: `<span>${_loc('DAGGERHEART.APPLICATIONS.DaggerheartMenu.chatMessageText')}</span>`,
+            rolls: [fearRoll],
+            flags: { daggerheart: { noButtons: true } }
+        });
+
+        await game.dice3d.waitFor3DAnimationByMessageID(message.id);
+
+        const automation = game.settings.get(CONFIG.DH.id, CONFIG.DH.SETTINGS.gameSettings.Automation).hopeFear;
+        if (automation.gm) {
+            ui.resources.updateFear(ui.resources.currentFear + fearRoll.total);
+        }
+    }
+
+    async getFallbackPartySize() {
+        const content = new foundry.data.fields.NumberField({
+            label: _loc('DAGGERHEART.APPLICATIONS.DaggerheartMenu.partSizePrompt.inputLabel'),
+            required: true,
+            nullable: false,
+            integer: true
+        }).toFormGroup({}, { name: 'partySize', localize: true }).outerHTML;
+
+        const partySize = await foundry.applications.api.DialogV2.prompt({
+            content: `
+                <span>${_loc('DAGGERHEART.APPLICATIONS.DaggerheartMenu.partSizePrompt.text')}</span>
+                ${content}
+            `,
+            rejectClose: false,
+            modal: true,
+            ok: { callback: (_, button) => button.form.elements.partySize.value },
+            window: {
+                title: _loc('DAGGERHEART.APPLICATIONS.DaggerheartMenu.partSizePrompt.title')
+            },
+            position: { width: 400 }
+        });
+
+        return partySize ? Number(partySize) : null;
     }
 }
